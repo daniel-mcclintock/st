1a20c8b168818807bb889da4338ddb22c3abf260
daniel mcclintock <daniel.mcclintock@impactdata.com.au>
2020-09-24 20:30:52 +1000

    Adjust font

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrains Mono:pixelsize=11";
+static char *font = "JetBrains Mono:size=11";
 
 /* disable bold, italic and roman fonts globally */
 int disablebold = 0;
